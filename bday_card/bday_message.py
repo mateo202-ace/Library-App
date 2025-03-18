03cd3609a3c05fc117f3eb9129d5be379c8b45c7
mateo202-ace <82839443+mateo202-ace@users.noreply.github.com>
2025-03-18 16:17:37 -0400

    Delete bday_card directory

--- bday_card/bday_message.py
+++
@@ -1,9 +0,0 @@
-import random
-
-bday_messages = ['Hope you have a very Happy Birthday! 🎈',
-'Its your special day, get out there and celebrate! 🎉',
-'You were born and the world got better, everybody wins! 🥳',
-'Have lots of fun on your special day! 🎂',
-'Another year of you going around the sun! 🌞']
-
-random_message = random.choice(bday_messages)
