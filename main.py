a880d4479ca0b179ca0585af98048c2ae83640b1
mateo202-ace <jmateo5916@gmail.com>
2025-05-13 09:48:43 -0400

    update books.csv with new entries and statuses

--- main.py
+++ main.py
@@ -9,37 +9,30 @@ csv_file = "/Users/juanmateo/Desktop/Library app/Library-App/data/books.csv"
 my_library = Library("Andreas Library", csv_file)
 
 books_to_add = [
-    ("The Shadows", "Alex North", "Dark - Mysterious - Suspense", "To Be Read"),
-    ("Baby X", "Kira Peikoff", "Thriller - Suspense - Dark", "To Be Read"),
-    ("I Only Read Murder", "Ian Ferguson, Will Ferguson", "Mysterious - Suspense", "To Be Read"),
-    ("What the Hell Did I Just Read", "David Wong, Jason Pargin", "Dark - Mysterious - Suspense", "To Be Read"),
-    ("Sheets", "Brenna Thummler", "Moving - Young Adult reading", "To Be Read"),
-    ("Delicates", "Brenna Thummler", "Moving - Young Adult reading", "To Be Read"),
-    ("Uzumaki", "Junji Ito", "Dark - Mysterious - Suspense - Manga", "To Be Read"),
-    ("The Nice House on the Lake Vol. 1", "James Tynion IV", "Horror - Suspense - Comic", "To Be Read"),
-    ("The Nice House on the Lake Vol. 2", "James Tynion IV", "Horror - Suspense - Comic", "To Be Read"),
-    ("Deadly Class Vol. 1: Reagan Youth", "Rick Remender", "Horror - Suspense - Comic", "To Be Read"),
-    ("Deadly Class Vol. 2: Kids of the Black Hole", "Rick Remender", "Horror - Suspense - Comic", "To Be Read"),
-    ("The Department of Truth #1", "James Tynion IV", "Horror - Suspense - Comic", "To Be Read"),
-    ("The Department of Truth #2", "James Tynion IV", "Horror - Suspense - Comic", "To Be Read"),
+    ("The Deepest Lake", "Andromeda Romano-Lax", "Dark - Mysterious - Thriller", "To Be Read"),
+    ("NightWatching", "Tracy Sierra", "Thriller - Mystery", "To Be Read"),
+    ("Listen For The Lie", "Amy Tintera", "Mysterious - Thriller", "To Be Read"),
+    ("Midnight at the Bright Ideas Bookstore", "Matthew Sullivan", "Dark - Mysterious", "To Be Read"),
+    ("Serial Killer Support Group", "Saratoga Schaefer", "Mysterious - Thriller - Suspense", "To Be Read"),
+    ("Home is Where the Bodies Are", "Jeneva Rose", "Mysterious - Thriller - Suspense", "To Be Read"),
 ]
 
 # # Add each book to the library
-# for title, author, genre, status in books_to_add:
-#     genres = genre.split(" - ")  # Split genres into a list
-#     new_book = Book(title, author, genres, status)
-#     my_library.add_book(new_book)
-#     print(f"✅ Added: {title} by {author}")
+#for title, author, genre, status in books_to_add:
+    #genres = genre.split(" - ")  # Split genres into a list
+    #new_book = Book(title, author, genres, status)
+    #my_library.add_book(new_book)
+    #print(f"✅ Added: {title} by {author}")
 
 # List all books in the library to confirm they were added
 #print("\nBooks in the library:")
-print(my_library.list_books())
+#print(my_library.list_books())
 
-results = my_library.move_to_dnf_list()
-print(results)
+#results = my_library.move_to_dnf_list()
+#print(results)
 
-# print("the next reandom book is..........:")
-# print(my_library.pick_random_book()) 
+print("the next reandom book is..........:")
+print(my_library.pick_random_book()) 
 
 # print("the next reandom book is:")
 # print(my_library.pick_random_book()) 
